027cac9a87c3c8756bad8b9868ef2e069b48a877
jonathanhansson <jonathanhanssondg@gmail.com>
2025-09-23 15:53:06 +0200

    Two new methods in data_processing which will 1. process data 2. get the features and target needed

--- src/taxipred/backend/data_processing.py
+++ src/taxipred/backend/data_processing.py
@@ -7,5 +7,23 @@ class TaxiData:
     def __init__(self):
         self.df = pd.read_csv(TAXI_CSV_PATH)
 
+    def preprocess_data(self):
+        categorical_cols = [
+            'Traffic_Conditions', 
+            'Weather', 
+            'Time_of_Day', 
+            'Day_of_Week'
+        ]
+
+        self.df = pd.get_dummies(self.df, columns=categorical_cols)
+
+        return self.df.dropna()
+    
+    def get_features_and_target(self, target='Trip_Price'):
+        df_processed = self.preprocess_data()
+        X, y = df_processed.drop(target, axis=1), df_processed[target]
+        
+        return X, y
+
     def to_json(self):
         return json.loads(self.df.to_json(orient = "records"))
