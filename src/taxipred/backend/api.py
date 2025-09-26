8870ba9a9af3f68279cda736aec2edc89554f945
jonathanhansson <jonathanhanssondg@gmail.com>
2025-09-26 10:20:27 +0200

    Created a predict endpoint, which will serve the frontend with a prediction

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -1,10 +1,23 @@
+import pandas as pd
 from fastapi import FastAPI
 from taxipred.backend.data_processing import TaxiData
+import joblib
 
 app = FastAPI()
 
+# instantiating a TaxiData object
 taxi_data = TaxiData()
+
+# loading the pretrained model
+model = joblib.load("src/taxipred/backend/taxi_price_model.pkl")
+
 
 @app.get("/taxi/")
 async def read_taxi_data():
     return taxi_data.to_json()
+
+@app.get("/taxi/prediction")
+async def predict(features: dict):
+    X = pd.DataFrame()
+    y_pred = model.predict(X)
+    return {"prediction": float(y_pred[0])}
