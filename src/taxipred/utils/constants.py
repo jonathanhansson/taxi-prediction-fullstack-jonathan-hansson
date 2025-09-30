a91023cba6957678660fc59c88a50a763bb6eb19
jonathanhansson <jonathanhanssondg@gmail.com>
2025-09-30 10:10:09 +0200

    Imported the os library to configure the pathways in the project.

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -1,5 +1,14 @@
-from importlib.resources import files
+import os
 
-TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+TAXI_CSV_PATH = os.path.join(BASE_DIR, "data", "taxi_trip_pricing.csv")
+ML_MODEL_PATH = os.path.join(BASE_DIR, "backend", "taxi_price_model.pkl")
 
 # DATA_PATH = Path(__file__).parents[1] / "data"
+
+if __name__ == '__main__':
+    print(BASE_DIR)
+    print(TAXI_CSV_PATH)
+    print(ML_MODEL_PATH)
+    
