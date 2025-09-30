94c8737753e12b124df8389ece40e06b77fe4762
jonathanhansson <jonathanhanssondg@gmail.com>
2025-09-30 10:07:44 +0200

    Since we will be receiving a JSON object, I use app.post instead of app.get

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -18,8 +18,14 @@ class TaxiFeatures(BaseModel):
 async def read_taxi_data():
     return taxi_data.to_json()
 
-@app.get("/taxi-price-prediction")
-async def predict(features: dict):
-    X = pd.DataFrame()
+"""
+Because we will be receving features (data) from the
+frontend as a JSON-object, we will use app.post() instead
+of app.get().
+"""
+@app.post("/taxi-price-prediction")
+async def predict(features_input: TaxiFeatures):
+    features_dict = features_input.features
+    X = pd.DataFrame([features_dict])
     y_pred = model.predict(X)
     return {"prediction": float(y_pred[0])}
