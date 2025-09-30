0d14a17c372d3dc77c32a7090965496c71f945a7
jonathanhansson <jonathanhanssondg@gmail.com>
2025-09-30 08:58:16 +0200

    Inherited BaseModel from Pydantic, to validate data

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -1,22 +1,24 @@
 import pandas as pd
 from fastapi import FastAPI
+from pydantic import BaseModel
+from taxipred.utils.constants import ML_MODEL_PATH 
 from taxipred.backend.data_processing import TaxiData
 import joblib
 
 app = FastAPI()
-
-# instantiating a TaxiData object
 taxi_data = TaxiData()
 
 # loading the pretrained model
-model = joblib.load("src/taxipred/backend/taxi_price_model.pkl")
+model = joblib.load(ML_MODEL_PATH)
 
+class TaxiFeatures(BaseModel):
+    features: dict
 
-@app.get("/taxi/")
+@app.get("/all-taxi-rides")
 async def read_taxi_data():
     return taxi_data.to_json()
 
-@app.get("/taxi/prediction")
+@app.get("/taxi-price-prediction")
 async def predict(features: dict):
     X = pd.DataFrame()
     y_pred = model.predict(X)
