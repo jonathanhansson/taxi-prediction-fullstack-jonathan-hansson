2b88930d346204ee6a34d6c78097136a564003c5
jonathanhansson <jonathanhanssondg@gmail.com>
2025-10-02 12:04:13 +0200

    Refactored a lot of the dashboard. Tab 1 now has the relevant table showing, tab 2 makes predictions on 4 input features from the end user, with the other features being default, tab 3 explains the 4 feature importances to end user

--- src/taxipred/frontend/dashboard.py
+++ src/taxipred/frontend/dashboard.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from taxipred.utils.helpers import read_api_endpoint
-from taxipred.utils.constants import ML_MODEL_PATH
+from taxipred.utils.constants import ML_MODEL_PATH, default_features
+from taxipred.training.visualize import plot_feature_importance
+import matplotlib.pyplot as plt
 import pandas as pd
 import joblib
-
+import os
 
 all_data = read_api_endpoint("all-taxi-rides")
 prediction = read_api_endpoint("taxi-price-prediction")
@@ -15,7 +17,7 @@ model = joblib.load(ML_MODEL_PATH)
 def main():
     st.markdown("# Taxi Prediction Dashboard")
 
-    tab1, tab2, tab3 = st.tabs(["Tab 1", "Tab 2", "Tab 3"])
+    tab1, tab2, tab3 = st.tabs(["Show all data", "Make your own prediction", "Feature importance"])
 
     with tab1:
         st.markdown("### All data tab")
@@ -23,65 +25,31 @@ def main():
             st.dataframe(all_data_df)
 
     with tab2: 
-        trip_distance_km = st.number_input("Trip distance in km: ", min_value=1, max_value=200, value=10)
-        time_of_day = st.selectbox(
-            "Time of day: ",
-            options=["Morning", "Afternoon", "Evening", "Night"])            
-        day_of_week = st.selectbox(
-            "Weekday or weekend: ",
-            options=["Weekday", "Weekend"]
-        )
-        passenger_count = st.number_input("Passenger count: ", min_value=1, max_value=4, value=2)        
-        traffic_conditions = st.selectbox(
-            "Low, medium or high traffic: ",
-            options=["Low", "Medium", "High"]
-        )      
-        weather = st.selectbox(
-            "Select weather: ",
-            options=["Clear", "Rain", "Snow"]
-        )                
-        base_Fare = st.number_input("Select base fare (2-5$): ", min_value=2, max_value=10, value=2)            
-        per_km_rate = st.number_input("$ per km: ", min_value=0.5, max_value=2.0, value=1.0)
-        per_minute_rate = st.number_input("$ per minute: ", min_value=0.1, max_value=1.0, value=0.1)        
-        trip_duration_minutes = st.number_input("Trip duration in minutes: ", min_value=5, max_value=200, value=15)
+        trip_distance_km = st.number_input("Trip distance in km: ", min_value=1, max_value=200, value=10)        
+        per_km_rate = st.number_input("Per km (0.5 - 2$): ", min_value=0.5, max_value=2.0, value=1.0)
+        per_minute_rate = st.number_input("Per minute (0.1 - 1$): ", min_value=0.1, max_value=1.0, value=0.1)        
+        trip_duration_minutes = st.number_input("Trip in minutes: ", min_value=5, max_value=200, value=15)
 
-        features_to_model = {
+        features_to_model = default_features.copy()
+
+        features_to_model.update({
             "Trip_Distance_km": trip_distance_km,
-            "Passenger_Count": passenger_count,
-            "Base_Fare": base_Fare,
             "Per_Km_Rate": per_km_rate,
             "Per_Minute_Rate": per_minute_rate,
-            "Trip_Duration_Minutes": trip_duration_minutes,
-            
-            # Since we did one hot encoding for these in the df, I will have to do the below
-            # Traffic_Conditions (one-hot-encoding)
-            "Traffic_Conditions_High": 1 if traffic_conditions == "High" else 0,
-            "Traffic_Conditions_Low": 1 if traffic_conditions == "Low" else 0,
-            "Traffic_Conditions_Medium": 1 if traffic_conditions == "Medium" else 0,
-
-            # Weather (one-hot-encoding)
-            "Weather_Clear": 1 if weather == "Clear" else 0,
-            "Weather_Rain": 1 if weather == "Rain" else 0,
-            "Weather_Snow": 1 if weather == "Snow" else 0,
-            
-            # Time_of_Day (one-hot-encoding)
-            "Time_of_Day_Afternoon": 1 if time_of_day == "Afternoon" else 0,
-            "Time_of_Day_Evening": 1 if time_of_day == "Evening" else 0,
-            "Time_of_Day_Morning": 1 if time_of_day == "Morning" else 0,
-            "Time_of_Day_Night": 1 if time_of_day == "Night" else 0,
-            
-            # Day_of_Week (one-hot-encoding)
-            "Day_of_Week_Weekday": 1 if day_of_week == "Weekday" else 0,
-            "Day_of_Week_Weekend": 1 if day_of_week == "Weekend" else 0    
-        }
+            "Trip_Duration_Minutes": trip_duration_minutes
+        })
 
         if st.button("Make prediction: "):
             X = pd.DataFrame([features_to_model])
             y_pred = model.predict(X)
-            st.write(y_pred[0])
+            st.write(f"{y_pred[0].round(1)}$ for this trip")
 
     with tab3:
-        st.write("Hej")
+        st.markdown("### Feature importance")
+        if st.button("ℹ️ What is this?"):
+            st.write("Feature importance shows which factors that the model thinks are most important for its price prediction")            
+        fig = plot_feature_importance(model, list(features_to_model.keys()))
+        st.pyplot(fig)
 
 
 
