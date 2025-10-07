4931f794db9e9725ec5555709dfe1a21c2cfd4a1
jonathanhansson <jonathanhanssondg@gmail.com>
2025-10-07 15:04:42 +0200

    Minor changes. E.g. convert_to_python_object() now displays the processed data instead of the raw data.

--- src/taxipred/backend/data_processing.py
+++ src/taxipred/backend/data_processing.py
@@ -8,7 +8,7 @@ class TaxiData:
         self.df = pd.read_csv(TAXI_CSV_PATH)
     
     def preprocess_data(self):
-        self.df = self.df.dropna()
+        df = self.df.dropna().copy()
 
         categorical_cols = [
             'Traffic_Conditions', 
@@ -17,10 +17,10 @@ class TaxiData:
             'Day_of_Week'
         ]
 
-        self.df = pd.get_dummies(self.df, columns=categorical_cols)
+        df = pd.get_dummies(df, columns=categorical_cols)
 
         # in the EDA I got the lowest MAE/MSE and highest R2 while dropping all the NaN columns 
-        return self.df
+        return df
     
     def get_features_and_target(self, target='Trip_Price'):
         df_processed = self.preprocess_data()
@@ -31,6 +31,7 @@ class TaxiData:
     # This was previously called "to_json()" but since it returns a 
     # Python object (list of dictionaries), we call it that
     def convert_to_python_object(self):
-        return json.loads(self.df.to_json(orient = "records"))
+        df_processed = self.preprocess_data()
+        return json.loads(df_processed.to_json(orient = "records"))
 
 
